3193ee1a2f4d6d42b7dbfc8f466eaa2b1ac74b7f
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2023-02-26 21:59:35 +0100

    wip: handshake processing

--- src/main/kotlin/HandshakeState.kt
+++ src/main/kotlin/HandshakeState.kt
@@ -12,28 +12,47 @@ data class HandshakeState(
     // assume that all required keys are provided
 ) {
 
-    // return InsufficientKeyMaterial | LastHandshakeMessage message c1 c2 | IntermediateHandshakeMessage message
+    // return InsufficientKeyMaterial | LastHandshakeMessage message c1 c2 | IntermediateHandshakeMessage message state
+    // writeMessage(payload: Payload: keyPair: KeyPair) // for new e messages
     fun writeMessage(payload: Payload): State<HandshakeState, Message>? =
         let {
             val pattern = messagePatterns.first()
             val result = pattern.fold(
-                State(
-                    symmetricState,
-                    Data(ByteArray(0))
-                ) as State<SymmetricState, Data>?
+                State(symmetricState, Data(ByteArray(0))) as State<SymmetricState, Data>?
             ) { state, token ->
-                state?.let { s ->
-                    when (token) {
-                        Token.E -> e
-                            ?.let { State(s.state.mixHash(it.public.data), s.value + it.public.data) }
-                            ?.let {
-                                it.state.encryptAndHash(payload.plainText)?.map { c -> it.value + c.data() }
-                                    ?: State(it.state, it.value + payload.data)
-                            }
+                when {
+                    state == null -> null
+                    token == Token.E && e != null -> let {
+                        val mixed = state.state.mixHash(e.public.data)
+                        val buffer = state.value + e.public.data
 
-                        else -> state
+                        // TODO later
+                        mixed.encryptAndHash(payload.plainText)?.map { c -> buffer + c.data() }
+                            ?: state.copy(value = buffer + payload.data)
                     }
+
+//                    State(
+//                        state.state.mixHash(e.public.data),
+//                        state.value + e.public.data
+//                    ).let {
+//                        it.state.encryptAndHash(payload.plainText)?.map { c -> it.value + c.data() }
+//                            ?: it.copy(value = it.value + payload.data)
+//                    }
+
+                    else -> state
                 }
+//                state?.let { s ->
+//                    when (token) {
+//                        Token.E -> e
+//                            ?.let { State(s.state.mixHash(it.public.data), s.value + it.public.data) }
+//                            ?.let {
+//                                it.state.encryptAndHash(payload.plainText)?.map { c -> it.value + c.data() }
+//                                    ?: State(it.state, it.value + payload.data)
+//                            }
+//
+//                        else -> state
+//                    }
+//                }
             }
             result?.let {
                 State(
@@ -42,6 +61,19 @@ data class HandshakeState(
                 )
             }
         }
+
+    sealed interface MessageResult {
+
+        object InsufficientKeyMaterial : MessageResult
+
+        data class IntermediateHandshakeMessage(val state: HandshakeState, val message: Message) : MessageResult
+
+        data class FinalHandshakeMessage(
+            val cipherState1: CipherState,
+            val cipherState2: CipherState,
+            val message: Message
+        ) : MessageResult
+    }
 
     enum class Role {
         INITIATOR, RESPONDER
