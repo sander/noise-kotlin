eb90c94229a8a3c905e499b6d72d664e71a10a0a
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2023-02-27 12:59:36 +0100

    wip: Noise_NK_25519_ChaChaPoly_SHA256

--- src/main/kotlin/HandshakePattern.kt
+++ src/main/kotlin/HandshakePattern.kt
@@ -2,14 +2,22 @@ package nl.sanderdijkhuis.noise
 
 import nl.sanderdijkhuis.noise.HandshakeState.Token
 
-data class HandshakePattern(val name: ProtocolName, val messagePatterns: List<List<Token>>) {
+data class HandshakePattern(val name: ProtocolName, val preSharedMessagePatterns: List<List<Token>>, val messagePatterns: List<List<Token>>) {
 
     companion object {
 
         val Noise_XN_25519_ChaChaPoly_SHA256 =
             HandshakePattern(
                 ProtocolName("Noise_XN_25519_ChaChaPoly_SHA256".toByteArray()),
+                listOf(),
                 listOf(listOf(Token.E), listOf(Token.E, Token.EE), listOf(Token.S, Token.SE))
+            )
+
+        val Noise_NK_25519_ChaChaPoly_SHA256 =
+            HandshakePattern(
+                ProtocolName("Noise_NK_25519_ChaChaPoly_SHA256".toByteArray()),
+                listOf(listOf(), listOf(Token.S)),
+                listOf(listOf(Token.E, Token.ES), listOf(Token.E, Token.EE))
             )
     }
 }
