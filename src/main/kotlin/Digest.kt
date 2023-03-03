e5ddf06fe2376f8ef41e144eaf7aea1348968bd4
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2023-03-03 12:10:57 +0100

    refactor: clean up hash functions

--- src/main/kotlin/Digest.kt
+++ src/main/kotlin/Digest.kt
@@ -4,8 +4,13 @@ package nl.sanderdijkhuis.noise
 value class Digest(val data: Data) {
 
     init {
-        require(data.size == HashFunction.HASH_SIZE)
+        require(data.size == SIZE)
     }
 
     val messageAuthenticationKey get() = MessageAuthenticationKey(data)
+
+    companion object {
+
+        val SIZE = Size(32)
+    }
 }
--- src/main/kotlin/SymmetricState.kt
+++ src/main/kotlin/SymmetricState.kt
@@ -5,7 +5,7 @@ data class SymmetricState(val cipherState: CipherState, val key: ChainingKey, va
     val cryptography get() = cipherState.cryptography
 
     fun mixKey(inputKeyMaterial: InputKeyMaterial) = let {
-        val result = HashFunction.deriveKeys(cryptography, key, inputKeyMaterial)
+        val result = deriveKeys(cryptography, key, inputKeyMaterial)
         copy(
             cipherState = CipherState(cryptography = cryptography, key = result.second.cipherKey),
             key = result.first.chainingKey
@@ -34,7 +34,7 @@ data class SymmetricState(val cipherState: CipherState, val key: ChainingKey, va
 
     fun split() = let {
         val zeroLen = InputKeyMaterial(Data.empty)
-        val temporaryKeys = HashFunction.deriveKeys(cryptography, key, zeroLen)
+        val temporaryKeys = deriveKeys(cryptography, key, zeroLen)
         val c1 = CipherState(cryptography, temporaryKeys.first.cipherKey)
         val c2 = CipherState(cryptography, temporaryKeys.second.cipherKey)
         Pair(c1, c2)
@@ -43,14 +43,46 @@ data class SymmetricState(val cipherState: CipherState, val key: ChainingKey, va
     companion object {
 
         fun initialize(cryptography: Cryptography, protocolName: ProtocolName) = let {
-            println("Initializing $cryptography $protocolName ${protocolName.data.size} ${HashFunction.HASH_SIZE}")
-            val h = if (protocolName.data.size <= HashFunction.HASH_SIZE)
-                Digest(Data(HashFunction.HASH_SIZE.byteArray { protocolName.data.value.getOrElse(it) { 0x00 } }))
+            println("Initializing $cryptography $protocolName ${protocolName.data.size} ${Digest.SIZE}")
+            val h = if (protocolName.data.size <= Digest.SIZE)
+                Digest(Data(Digest.SIZE.byteArray { protocolName.data.value.getOrElse(it) { 0x00 } }))
             else
                 cryptography.hash(protocolName.data)
             val ck = ChainingKey(h)
             val state = CipherState(cryptography)
             SymmetricState(state, ck, HandshakeHash(h))
+        }
+
+        private val BLOCK_SIZE = Size(64)
+
+        /** https://www.ietf.org/rfc/rfc2104.txt */
+        private fun authenticateMessage(cryptography: Cryptography, key: MessageAuthenticationKey, data: Data) = let {
+
+            fun block(init: (Int) -> Byte) = Data(ByteArray(BLOCK_SIZE.value, init))
+
+            val keyData =
+                if (key.data.size <= BLOCK_SIZE)
+                    block { key.data.value.getOrElse(it) { 0x00 } }
+                else cryptography.hash(key.data).data
+            val innerPadding = block { 0x36 }
+            val outerPadding = block { 0x5c }
+
+            val digest =
+                cryptography.hash(keyData.xor(outerPadding) + cryptography.hash(keyData.xor(innerPadding) + data).data)
+            MessageAuthenticationData(digest)
+        }
+
+        internal fun deriveKeys(cryptography: Cryptography, key: ChainingKey, material: InputKeyMaterial) = let {
+            val temporaryKey =
+                authenticateMessage(cryptography, key.digest.messageAuthenticationKey, material.data)
+            val output1 =
+                authenticateMessage(cryptography, temporaryKey.digest.messageAuthenticationKey, Data(byteArrayOf(0x01)))
+            val output2 = authenticateMessage(
+                cryptography,
+                temporaryKey.digest.messageAuthenticationKey,
+                output1.digest.data + Data(byteArrayOf(0x02))
+            )
+            Pair(output1, output2)
         }
     }
 }
