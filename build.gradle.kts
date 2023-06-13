17ba096cdf4b25b08cd44006baed0da1d72a04a6
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2023-06-13 07:58:45 +0200

    ci: also publish snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,14 +91,6 @@ publishing {
             }
         }
     }
-    repositories {
-        maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            credentials(PasswordCredentials::class.java)
-        }
-    }
 }
 
 nexusPublishing {
